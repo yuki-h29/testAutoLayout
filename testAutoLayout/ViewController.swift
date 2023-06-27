42249758a88cbf701eebcc70e51bb9ad8d4e2453
yuki-h29 <yuki.hirano29@gmail.com>
2023-06-27 21:59:37 +0900

    Initial Commit

---
+++ testAutoLayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testAutoLayout
+//
+//  Created by 平野裕貴 on 2023/06/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
