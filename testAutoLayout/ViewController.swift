bdcd498c4ab3e65dcc8bd83dbd42b3487a7525eb
yuki-h29 <yuki.hirano29@gmail.com>
2023-06-27 23:51:34 +0900

    製造：　ScrollViewにラベルを追加して、動的に表示エリア(高さ)を変更していくサンプル

--- testAutoLayout/ViewController.swift
+++ testAutoLayout/ViewController.swift
@@ -14,6 +14,31 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var contentView: UIView!
+    @IBOutlet weak var contentHeightConstraint: NSLayoutConstraint!
+    
+    
+    // ラベルを追加するためのカウンター
+    var count: Int = 0
+    @IBAction func testButton(_ sender: Any) {
+        // 新しいラベルを生成
+        let newLabel = UILabel()
+        newLabel.translatesAutoresizingMaskIntoConstraints = false
+        newLabel.text = "Label \(count)"
+        
+        // ラベルをContentViewに追加
+        contentView.addSubview(newLabel)
 
+        // ラベルに対する制約を設定
+        newLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20).isActive = true
+        newLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: CGFloat((20 * count))).isActive = true
+
+        // ContentViewの高さを更新
+        count += 1
+        contentHeightConstraint.constant = CGFloat(20 * count)
+    }
+    
+    
 }
 
